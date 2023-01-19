45f3f74bb7e196a02895d30e00b4da55a7836576
JoaorGuimaraes <joaoguimaraes705@gmail.com>
2023-01-19 15:37:48 -0300

    Sistema de mensagem e página de about prontos

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,8 @@
+import { MomentService } from './../../../services/moment.service';
+import { Moment } from './../../../Moment';
 import { Component } from '@angular/core';
+import { MessagesService } from 'src/app/services/messages.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-moment',
@@ -9,7 +13,21 @@ export class NewMomentComponent {
 
   btnText= "Compartilhar!";
 
-  createdHandler(event: any) {
-    console.log("Deu bom")
+  constructor(private momentService: MomentService, private messagesService: MessagesService, private router: Router){}
+
+  async createdHandler(moment: Moment) {
+    const formData = new FormData();
+    formData.append("title", moment.title);
+    formData.append("description", moment.description);
+    
+    if(moment.image){
+      formData.append("image", moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    this.messagesService.add("Momento criado com sucesso!")
+
+    this.router.navigate(['/'])
   }
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -1,9 +1,22 @@
+import { Moment } from './../Moment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { enviroment } from 'src/enviroments/enviroment';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MomentService {
 
-  constructor() { }
+  private baseApiUrl = enviroment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
+ 
+  constructor(private http: HttpClient) { }
+
+  createMoment(formData: FormData): Observable<FormData> {
+    return this.http.post<FormData>(this.apiUrl, formData);
+  }
 }
